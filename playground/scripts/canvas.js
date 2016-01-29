47e9b50d3c40ef2d0de1b323a883dc041f5006b0
wei zhang <zhangweikop@gmail.com>
2016-01-29 03:40:08 -0600

    add the move feature of the object on the page

--- playground/scripts/canvas.js
+++ playground/scripts/canvas.js
@@ -1,26 +1,30 @@
-function PlayGroundCanvasView(canvasWidth, canvasHeight){
-	var classRobotPlayground    = 'robot-playground';
+
+function makeMyBoardView(rootDom, rows, columns){
 	var classRobotPlaygroundRow = 'robot-playground-row';
 	var classRobotPlaygroundCell = 'robot-playground-cell';
-	var classRobotPlaygroundCharacter = 'robot-playground-character'
-	var robotElement = '<div class = "' + classRobotPlaygroundCharacter + '" style = "maxHeight: 100%; height:100%; width: 100%; maxWidth: 100%;">'+
-									'<img src="/playground/images/robot.png" style = "maxHeight: 100%; height:100%; width: 100%; maxWidth: 100%;">'
-									+'</div>';
-	var playgroundClearFix = '<div style="clear:both"></div>';								
-	function initialCanvas(width, height) {
-		for(var i = 0 ; i < height; i++) { 
+	var board = [];
+	var boardObjects = {};
+	var rootContainer = $(rootDom);
+	function initial(rootDom, height, width) {
+		var playgroundClearFix = '<div style="clear:both"></div>';	
 
-			$('.' + classRobotPlayground).css('width', $('.' + classRobotPlayground).parent().width());
-			$('.' + classRobotPlayground).append('<div class = "' + classRobotPlaygroundRow + '"></div>');
-
+		board = new Array(height);
+		for (var i = 0; i < height; i++) {
+		  board[i] = new Array(width);
+		  for (var j = 0; j < width; j++) {
+		    board[i][j] = {};
+		  }
 		}
-		$('.' + classRobotPlaygroundRow).each(function(index) {
+		for(var i = 0 ; i < height; i++) { 
+			rootContainer.css('width', rootContainer.parent().width());
+			rootContainer.append('<div class = "' + classRobotPlaygroundRow + '"></div>');
+		}
+		rootContainer.children().each(function(rowIndex) {
 				var row = $(this);
-			  for (var i = 0; i < width; i++) {
-
+			  for (var colIndex = 0; colIndex < width; colIndex++) {
 		    	var cell = $('<div class = "' + classRobotPlaygroundCell + '"> </div>');
-		    	if(i == 0) {
-		    		$(cell).append('<div style = "position:absolute">' + (index + 1) + '</div>');	
+		    	if(colIndex == 0) {
+		    		cell.append('<div style = "position:absolute">' + (rowIndex + 1) + '</div>');	
 			  	} 
 		    	cell.css( 'width', ( row.width()/width) + 'px' );
 		    	cell.css( 'maxWidth', ( row.width()/width) + 'px' );
@@ -33,51 +37,97 @@ function PlayGroundCanvasView(canvasWidth, canvasHeight){
 		    	cell.css( 'border', '1px solid #ddd');
 		    	cell.css('background-color', 'white');
 		    	row.css( 'maxHeight', ( row.width() / width) + 'px' );
-
+		    	board[rowIndex][colIndex] = cell[0];
 		    	row.append(cell);
-		    	if (i == width-1) {
+		    	if (colIndex == width-1) {
 		    		row.append(playgroundClearFix);
 		    	}
 		    }
 		});
+		
+
+ 	    $(window).on("resize", resizeThrottler);
+
+	    var resizeTimeout;
+	    function resizeThrottler() {
+	    	// ignore resize events as long as an actualResizeHandler execution is in the queue
+	    	if ( !resizeTimeout ) {
+	      		resizeTimeout = setTimeout(function() {
+	        	resizeTimeout = null;
+	        	actualResizeHandler();	     
+	       		// The actualResizeHandler will execute at a rate of 15fps
+	       		}, 20);
+	    	}
+	  	}
+
+	    function actualResizeHandler() {
+	    	// handle the resize event
+	    	rootContainer.css('width', rootContainer.parent().width());
+	    	rootContainer.children('.' + classRobotPlaygroundRow).each( function(index){
+				var row = $(this);
+				row.children('.' + classRobotPlaygroundCell).css( {'width' : ( row.width()/columns) + 'px', 'height' : ( row.width() / columns ) + 'px'});
+	    	})
+	  	}
 	}
-	function initialCharacters(x, y) {
-		var rows = $('.' + classRobotPlaygroundRow);
-		var robotCell = $($(rows[x]).children()[y]);
-		var robotCharacter =  $(robotElement);
-		robotCharacter.data('coordinateX', x);
-		robotCharacter.data('coordinateY', y);
-		robotCell.append(robotCharacter);
+
+
+	function createElement(x, y, elementName, element) {
+		var elementContainer = board[x][y];
+		var newElement;
+		if (!!elementName && !!element) {
+			if (boardObjects.hasOwnProperty(elementName)) {
+				return false;
+			}
+			newElement = $(element);
+			$(elementContainer).append(newElement);
+			boardObjects[elementName] = newElement;
+			return true;
+		} 
+		return false;
 	}
-	
-	initialCanvas(canvasWidth, canvasHeight);
-	initialCharacters(3, 2);
-	
-//handle resize for the playground canvas
-	(function() {
 
-	  $(window).on("resize", resizeThrottler);
-
-	  var resizeTimeout;
-	  function resizeThrottler() {
-	    // ignore resize events as long as an actualResizeHandler execution is in the queue
-	    if ( !resizeTimeout ) {
-	      resizeTimeout = setTimeout(function() {
-	        resizeTimeout = null;
-	        actualResizeHandler();
-	     
-	       // The actualResizeHandler will execute at a rate of 15fps
-	       }, 20);
-	    }
-	  }
-
-	  function actualResizeHandler() {
-	    // handle the resize event
-	    $('.' + classRobotPlayground).css('width', $('.' + classRobotPlayground).parent().width());
-	    $('.' + classRobotPlaygroundRow).each( function(index){
-			var row = $(this);
-			row.children('.' + classRobotPlaygroundCell).css( {'width' : ( row.width()/canvasWidth) + 'px', 'height' : ( row.width() / canvasWidth ) + 'px'});
-	    })
-	  }
-	}());
+	function moveElement(x, y, newx, newy, objectName, time) {
+		if (newx >= columns || newy >= rows) {
+			return false;
+		}
+		var oldContainer = board[x][y];
+		var newContainer = board[newx][newy];
+		var element = boardObjects[objectName];
+		var startPoint = $(element).offset();
+ 		var endPoint = $(newContainer).offset();
+ 		var xOffset = endPoint.left - startPoint.left;
+ 		var yOffset = endPoint.top - startPoint.top;
+ 		var timeInterval = 30;
+ 		var stepx = xOffset/(time/timeInterval);
+ 		var stepy = yOffset/(time/timeInterval);
+		function translate(i, j, xOffset, yOffset){ 
+			var x = i + stepx;
+			var y = j + stepy;
+			if ( (x - xOffset)*(i - xOffset) <=0) {
+				x = xOffset;
+			}
+			if ((y - yOffset)*(j - yOffset) <=0 ) {
+				y = yOffset;
+			}
+			if (x != xOffset || y != yOffset) {
+				$(element).css({transform: 'translate(' + x + 'px,' + y + 'px)'});
+				setTimeout(function(){
+					translate(x, y, xOffset, yOffset);
+				}, timeInterval);
+			} else {
+				$(element).css({transform: 'none'});
+				$(element).appendTo($(newContainer));
+			}			
+		}
+		translate(0, 0, xOffset, yOffset);
+		return true;
+	}
+	initial(rootDom, rows, columns);
+	return {
+		rootElement: rootDom,
+		board: board,
+		createElementAtBoard: createElement,
+		boardObjects: boardObjects,  
+		MoveElementInsideBoard: moveElement
+	}
 }
--- playground/scripts/startup.js
+++ playground/scripts/startup.js
@@ -1,12 +1,9 @@
 //viewcontroller and view are view realted
 function ViewController(){
-	this.view = new html5canvasView();
-	this.view.CreateCharacter('robot', 4, 6);;
-	this.view.MoveCharacter();
-	this.view.moveRobot = function(x, y ){};
-
+	this.view = html5canvasView(6, 8, 2, 1);
 }
 
+var WebApplication = WebApplication||{};
 //the service will be used by the controller.the execution service will connect to the execution engine 
 // and send data to each other
 function ExecutionService(){
@@ -15,16 +12,28 @@ function ExecutionService(){
 }
 
 
-function html5canvasView()
+function html5canvasView(r, c, x, y) {
+	var rootDom = $('#robot-playground')[0];
+ 	var view = makeMyBoardView(rootDom, r, c);
+	var classRobotPlaygroundCharacter = 'robot-playground-character';
+	var robotElement = '<div class = "' + classRobotPlaygroundCharacter + '" style = "maxHeight: 100%; height:100%; width: 100%; maxWidth: 100%;">'+
+									'<img src="/playground/images/robot.png" style = "maxHeight: 100%; height:100%; width: 100%; maxWidth: 100%;">'
+									+'</div>';
+ 	view.createElementAtBoard(x, y, 'robot', robotElement);
+ 	return view;
+}
 
 	//beresponsible for the databinding between the property and the dom element
 
-}
 html5canvasView.prototype.CreateCharacter = function(){};
 html5canvasView.prototype.MoveCharacter = function(){};
 
 $(document).ready(function(){
-	PlayGroundCanvasView(8, 6);
+	 var controller = new ViewController();
+	 WebApplication.boardController = controller;
+	setTimeout(function(){
+		console.log(WebApplication.boardController.view.MoveElementInsideBoard(2,1,2,3, 'robot',1000));
+	}, 1000); 
 })
 
 
@@ -35,5 +44,6 @@ function ExecutionEngine(){
 
 // the execution driver.the name is cute
 function ExecutionDriver(){
+	// each command will take 1 second
 
 }
